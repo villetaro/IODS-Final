53bd12f8e62e5a95b01fc817b6a8fe75f3035d37
villetaro <ville.t.aro@helsinki.fi>
2017-02-28 20:11:19 +0200

    Uutta hommaa

--- alcdata.R
+++ alcdata.R
@@ -1,3 +1,6 @@
+## Ville Aro, ville.t.aro@helsinki.fi
+## March 1, 2017
+
 library(dplyr)
 
 alc <- read.table("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/alc.txt ", header=T, sep=",")
@@ -5,3 +8,19 @@ alc <- read.table("http://s3.amazonaws.com/assets.datacamp.com/production/course
 dim(alc)
 
 colnames(alc)
+
+## The data has 382 observations and 35 different variables. I decided to only keep the 
+## variables that I find interesting.
+
+keep <- c("school", "sex", "age", "goout", "health", "absences", "G1", "G2", "G3", "alc_use", "high_use")
+
+alcdata <- dplyr::select(alc, one_of(keep))
+
+str(alcdata)
+summary (alcdata)
+
+## Now I have all the interesting variables in my alcdata, so I write the table
+## and move on to the next part.
+
+
+write.csv(alcdata, file="alcdata", row.names = T)
